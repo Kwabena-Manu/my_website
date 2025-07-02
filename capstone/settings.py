1afc14ff7f5d8144768bcd79c5432b9028616a5c
Kwabena Manu <kwabenamanu61@outlook.com>
2025-07-02 00:08:33 -0700

    setup for deployment

--- capstone/settings.py
+++ capstone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'mywebsite/static',
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -146,7 +147,7 @@ LOGGING = {
     'handlers': {
         'file':{
             'level': 'INFO',
-            'class': 'logging.Filehandler',
+            'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, 'django_debug.log'),
         }
     },
